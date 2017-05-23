51d22fdabff68e3263f8f1a3b3780bc2bb9c9da2
Tosuke <tasukeprg@gmail.com>
2017-05-23 21:16:27 +0900

    :bangbang: modify '__emoji-cli_emoji' binding

--- key_bindings.fish
+++ key_bindings.fish
@@ -1,7 +1,7 @@
 bind \cs "__emoji-cli_name"
-bind \ce "__emoji-cli_emoji"
+bind \cx "__emoji-cli_emoji"
 
 if bind -M insert >/dev/null ^/dev/null
   bind -M insert \cs "__emoji-cli_name"
-  bind -M insert \ce "__emoji-cli_emoji"
+  bind -M insert \cx "__emoji-cli_emoji"
 end

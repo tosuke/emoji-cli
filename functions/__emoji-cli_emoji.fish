7a3b40aebdb6d252deb28d10033e6d16f9fbd959
Tosuke <tasukeprg@gmail.com>
2017-05-23 21:00:06 +0900

    :bug: fix emoji regex

--- functions/__emoji-cli_emoji.fish
+++ functions/__emoji-cli_emoji.fish
@@ -1,5 +1,5 @@
 function __emoji-cli_emoji
   select-emoji --emoji | read -l result
   commandline -it -- $result
-  commandline -it -- $result
+  commandline -f repaint
 end
--- functions/select-emoji.fish
+++ functions/select-emoji.fish
@@ -1,6 +1,11 @@
 function select-emoji -d "Select emoji using selector"
+  set -q EMOJI_SELECTOR; or set -l EMOJI_SELECTOR "fzf"
+  
   set -l mode "name"
-  __emoji-cli_dict_json | jq -r "to_entries | sort_by(.key) | map(\"\(.value):\(.key):\") | .[]" | fzf | read -l result
+  __emoji-cli_dict_json \
+    | jq -r "to_entries | sort_by(.key) | map(\"\(.value) :\(.key):\") | .[]" \
+    | eval $EMOJI_SELECTOR \
+    | read -l result
 
   getopts $argv | while read -l opt
     switch $opt
@@ -13,11 +18,10 @@ function select-emoji -d "Select emoji using selector"
     end
   end
 
-  set -l output
   switch $mode
     case "name"
       echo $result | sed -E "s/^[^:]+//"
     case "emoji"
-      echo $result | sed -E "s/^([^:]+).*/\1/"
+      echo $result | sed -E "s/^([^\ ]+).*/\1/"
   end
 end

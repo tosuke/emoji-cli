8f378f745b455e0f490e68c0a16b43da77c61658
Tosuke <tasukeprg@gmail.com>
2018-01-22 16:51:53 +0900

    :gear: fix

--- generate.js
+++ generate.js
@@ -6,6 +6,6 @@ const list = emoji
   .filter(e => e.emoji)
   .map(e => e.aliases.map(a => `"${e.emoji} :${a}:"`).join('\\n'))
   .join('\\n')
-const src = fs.readFileSync('./template.fish', { encoding: 'utf-8' })
+const src = fs.readFileSync('./__emoji-cli.fish.template', { encoding: 'utf-8' })
 const dest = src.replace('@emoji', list)
 fs.writeFileSync('./functions/__emoji-cli.fish', dest)

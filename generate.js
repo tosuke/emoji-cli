8caf94a56508e2ddc819efcb7236d15015809f8e
Tosuke <tasukeprg@gmail.com>
2018-01-24 16:40:37 +0900

    :gear: use external dict

--- generate.js
+++ generate.js
@@ -4,8 +4,6 @@ const emoji = require('./emoji.json')
 
 const list = emoji
   .filter(e => e.emoji)
-  .map(e => e.aliases.map(a => `"${e.emoji} :${a}:"`).join('\\n'))
-  .join('\\n')
-const src = fs.readFileSync('./__emoji-cli.fish.template', { encoding: 'utf-8' })
-const dest = src.replace('@emoji', list)
-fs.writeFileSync('./functions/__emoji-cli.fish', dest)
+  .map(e => e.aliases.map(a => `${a}\t${e.emoji}`).join('\n'))
+  .join('\n')
+fs.writeFileSync('./functions/__emoji-cli_emoji.tsv', list)

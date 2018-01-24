87c3ce8ac7b822d12e5d2d06167e445055e43b50
Tosuke <tasukeprg@gmail.com>
2018-01-24 16:43:27 +0900

    :wrench: fix string command

--- functions/__emoji-cli.fish
+++ functions/__emoji-cli.fish
@@ -12,18 +12,18 @@ function __emoji-cli -d 'Emoji completion on the command line'
   set -l buf (commandline -t)
   set -l cursor (commandline -C)
   set -l lbuf (commandline -tc)
-  set -l rbuf (string sub --start=(math (string length $lbuf) +1) $buf)
+  set -l rbuf (string sub --start=(math (string length -- $lbuf) +1) $buf)
 
   set -l query ''
   set -l mode 'insert'
   if test -n $lbuf
-    set -l lquery (string match -r ':?[a-z0-9\+\-\_]+$' $lbuf)
-    set lbuf (string sub --length=(math (string length $lbuf) - (string length $lquery; or echo 0)) $lbuf)
-    set -l rquery (string match -r '^[a-z0-9\+\-\_]+:?' $rbuf)
-    set rbuf (string sub --start=(math (string length $rquery; or echo 0) +1) $rbuf)
+    set -l lquery (string match -r ':?[a-z0-9\+\-\_]+$' -- $lbuf)
+    set lbuf (string sub --length=(math (string length -- $lbuf) - (string length -- $lquery; or echo 0)) -- $lbuf)
+    set -l rquery (string match -r '^[a-z0-9\+\-\_]+:?' -- $rbuf)
+    set rbuf (string sub --start=(math (string length -- $rquery; or echo 0) +1) -- $rbuf)
     set query "$lquery$rquery"
 
-    if string length -q $rquery
+    if string length -q -- $rquery
       set mode 'replace'
     end
   end
@@ -37,7 +37,7 @@ function __emoji-cli -d 'Emoji completion on the command line'
   if test -n $emoji
     set nbuf "$lbuf$emoji$rbuf"
     if test $mode = 'insert'
-      set cursor (math $cursor + (string length $nbuf) - (string length $buf))
+      set cursor (math $cursor + (string length -- $nbuf) - (string length -- $buf))
     end
     commandline -rt $nbuf
     commandline -C $cursor
